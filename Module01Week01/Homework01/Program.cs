e70475915daf8f0e0fbd4b6dfeff6a8fa05f1643
Feliciu Svabu <feliciu.svabu@outlook.com>
2018-05-12 19:09:00 +0300

    Cod final.

--- Module01Week01/Homework01/Program.cs
+++ Module01Week01/Homework01/Program.cs
@@ -10,30 +10,32 @@ namespace Homework01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the number");
+            Console.WriteLine("Guess the number\n");
             Random rndm = new Random();
             int number = rndm.Next(0, 100);
             int nr = 0;
-            
+
             Console.WriteLine("Please insert a number bweteen 0 and 100");
+
             do
             {
                 string str = Console.ReadLine();
-                nr = int.Parse(str);
-                if (nr > 100)
+
+                if (!int.TryParse(str, out nr))
                 {
-                    Console.WriteLine("Inser a number bweteen 0 and 100");
-                } else if (nr < number)
+                    Console.WriteLine("Error. Enter a number please!!!!");
+                }
+                else if (nr < number)
                 {
-                    Console.WriteLine("The riht number is bigger");
+                    Console.WriteLine("The correct number is greater");
                 }
                 else if (nr == number)
                 {
-                    Console.WriteLine($"Congrtulation, the right number is {number}");
+                    Console.WriteLine($"Congratulation, the correct number is {number}");
                 }
                 else
                 {
-                    Console.WriteLine("The right number is lower");
+                    Console.WriteLine("The correct number is lesser");
                 }
             } while (nr != number);
         }
