beab04bb9ab0033cef521cfd244996af88284175
Feliciu Svabu <feliciu.svabu@outlook.com>
2018-05-12 18:22:41 +0300

    Aplicatia Ghiceste Numarul

--- Module01Week01/Homework01/Program.cs
+++ Module01Week01/Homework01/Program.cs
@@ -10,6 +10,39 @@ namespace Homework01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Guess the number");
+            Random rndm = new Random();
+            int number = rndm.Next(0, 100);
+            int nr = 0;
+
+
+            Console.WriteLine("Please insert a number bweteen 0 and 100");
+            do
+            {
+                string str = Console.ReadLine();
+                nr = int.Parse(str);
+                if (nr > 100)
+                {
+                    Console.WriteLine("Inser a number bweteen 0 and 100");
+                } else if (nr < number)
+                {
+                    Console.WriteLine("The riht number is bigger");
+                }
+                else if (nr == number)
+                {
+                    Console.WriteLine($"Congrtulation, the right number is {number}");
+                }
+                else
+                {
+                    Console.WriteLine("The right number is lower");
+                }
+            } while (nr != number);
+
+
+
+
+
+
         }
     }
 }
